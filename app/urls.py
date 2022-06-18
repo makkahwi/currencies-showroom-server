d387509a08bce180dc0ca888aec348b881b81321
makkahwi <suhaibahmadai@hotmail.com>
2022-06-18 10:17:35 +0300

    Build API Configs

---
+++ app/urls.py
@@ -0,0 +1,42 @@
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
+from rest_framework_simplejwt import views as jwt_views
+
+from app.viewset import (
+    CurrencyListView,
+    CurrencyCreateView,
+    CurrencyDetailView,
+    CurrencyUpdateView,
+    CurrencyDestroyView,
+)
+
+urlpatterns = [
+    path("token/obtain", jwt_views.TokenObtainPairView.as_view(), name="JWT Obtain"),
+    path("token/refresh", jwt_views.TokenRefreshView.as_view(), name="JWT Refresh"),
+    path("api/v1/currencies", CurrencyListView.as_view(), name="Currency API List"),
+    path(
+        "api/v1/currencies/create",
+        CurrencyCreateView.as_view(),
+        name="Currency API Create",
+    ),
+    path(
+        "api/v1/currencies/<int:pk>",
+        CurrencyDetailView.as_view(),
+        name="Currency API Detail",
+    ),
+    path(
+        "api/v1/currencies/<int:pk>/update",
+        CurrencyUpdateView.as_view(),
+        name="Currency API Update",
+    ),
+    path(
+        "api/v1/currencies/<int:pk>/delete",
+        CurrencyDestroyView.as_view(),
+        name="Currency API Delete",
+    ),
+    re_path(
+        r"^.*$",
+        RedirectView.as_view(url="api/v1/currencies", permanent=False),
+        name="Redirect",
+    ),
+]
---
+++ app/viewset.py
@@ -0,0 +1,35 @@
+from rest_framework import generics
+
+from app.models import Currency
+
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CurrencySerializer
+
+
+class CurrencyListView(generics.ListAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+
+
+class CurrencyCreateView(generics.ListCreateAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class CurrencyDetailView(generics.RetrieveAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class CurrencyUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class CurrencyDestroyView(generics.RetrieveDestroyAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+    permission_classes = (IsOwnerOrReadOnly,)
